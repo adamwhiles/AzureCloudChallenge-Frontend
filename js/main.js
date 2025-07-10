74b69ffd8d2ff51d6128548079a87e583f610a59
Adam <awhiles2011@gmail.com>
2025-07-09 21:19:52 -0500

    update site to use new function app

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ span.textContent = "Loading Count...";
 // Define a function to fetch the value from the endpoint
 function fetchValue() {
   // Use the fetch API to make a GET request
-  fetch("https://app-heyitsadam.azurewebsites.net/api/http_trigger?code=XYsh4VlO4BnmUQ-hZRcbODcc84pWg8bZKhMKMtM2LtyfAzFuSYSwUQ==")
-    .then(response => response.json()) // Parse the response as JSON
-    .then(data => {
+  fetch(
+    "https://func-heyitsadam.azurewebsites.net/api/http_trigger?code=QIQ_hAW3vKiL_26IyWMgtUQ7n-O8P-3JOjmuLia5-rmAAzFusufUOw=="
+  )
+    .then((response) => response.json()) // Parse the response as JSON
+    .then((data) => {
       // Update the span text with the data value
       span.textContent = data;
     })
-    .catch(error => {
+    .catch((error) => {
       // Handle any errors
       console.error(error);
     });
